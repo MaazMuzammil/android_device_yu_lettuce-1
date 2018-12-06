d480c0149659b7d511cee6eefb836d14a31dc3da
Adithya R <gh0strider.2k18.reborn@gmail.com>
2018-12-06 10:23:34 +0530

    lettuce: fs_config: Update config.fs from LA.UM.7.7.r1-01900-8x09.0
    - Not adding rules for services which we don't have

--- fs_config/config.fs
+++ fs_config/config.fs
@@ -1,3 +1,6 @@
+[AID_VENDOR_QTI_DIAG]
+value:2901
+
 [AID_VENDOR_QCOM_DIAG]
 value:2950
 
@@ -6,6 +9,12 @@ value:2951
 
 [AID_VENDOR_RFS_SHARED]
 value:2952
+
+[system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: BLOCK_SUSPEND NET_ADMIN
 
 [system/vendor/bin/imsdatadaemon]
 mode: 0755
@@ -18,3 +27,15 @@ mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
+
+[system/vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
+[system/vendor/bin/cnd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
